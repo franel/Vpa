026d452dec3efa564431fdfdb0f1a2bef418beb3
franel <38464689+franel@users.noreply.github.com>
2025-05-01 09:20:01 +0100

    Create page.tsx

---
+++ modules/thememanager/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import ThemeSwitcher from './ThemeSwitcher';
+
+export default function ThemeManagerPage() {
+  return (
+    <div className="p-4">
+      <ThemeSwitcher />
+    </div>
+  );
+}
