d8a32d2acca51182994ac75c2a675f72b19210e4
franel <38464689+franel@users.noreply.github.com>
2025-05-01 09:14:05 +0100

    Create page.tsx

---
+++ modules/pluginloader/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import PluginLoader from './PluginLoader';
+
+export default function PluginLoaderPage() {
+  return (
+    <div className="p-4">
+      <PluginLoader />
+    </div>
+  );
+}
